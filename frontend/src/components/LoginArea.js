c5064f4655d5653b1175ffe94e1c576d3493ebd2
Rawisara Saman <rawiisara@gmail.com>
2022-05-09 20:01:30 +0200

    [frontend] login page added

--- frontend/src/components/LoginArea.js
+++ frontend/src/components/LoginArea.js
@@ -2,7 +2,17 @@ import React from 'react'
 
 const LoginArea = () => {
   return (
-    <div>Signin</div>
+    <section className='register-wrapper'>
+    <img className='logo' src='./images/logo.svg' alt='logo' />
+    <form className='register-form'>
+        <label htmlFor="signup-form"></label>
+        <p className='create-account-text'>Create a free account to connect with thousands of readers</p>
+        <input type='text' placeholder='Email address or username' required/>
+        <input type='password' placeholder='Password' required/>
+        <button type='submit'>LOGIN</button>
+    </form>
+    <p>Already have an account? <span className='to-signin-page'>Sign in</span></p>
+</section>
   )
 }
 
