008ee42f1f431cacd2c4a2d6a4782b1a3ad07252
Rawisara Saman <rawiisara@gmail.com>
2022-05-17 13:27:49 +0200

    api routes added

--- frontend/src/components/Register.js
+++ frontend/src/components/Register.js
@@ -1,17 +1,52 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Register.css'
 
 
 const Register = () => {
+    const [newUser, setNewUser] = useState({
+        email: '',
+        username: '',
+        password: ''
+    })
+
+    const onNewUserChange = (event) => {
+        event.preventDefault()
+        const { name, value } = event.target
+        setNewUser((prev) => {
+            return {
+                ...prev,
+                [name]: value
+            }
+        })
+    }
+
     return (
         <section className='register-wrapper'>
             <img className='logo' src='./images/logo.svg' alt='logo' />
             <form className='register-form'>
                 <label htmlFor="signup-form"></label>
                 <p className='create-account-text'>Create a free account to connect with thousands of readers</p>
-                <input type='text' placeholder='Email address' required/>
-                <input type='text' placeholder='Username' required/>
-                <input type='password' placeholder='Password' required/>
+                <input
+                    type='text'
+                    placeholder='Email address'
+                    onChange={onNewUserChange}
+                    value={newUser.email}
+                    required
+                />
+                <input
+                    type='text'
+                    placeholder='Username'
+                    onChange={onNewUserChange}
+                    value={newUser.username}
+                    required
+                />
+                <input
+                    type='password'
+                    placeholder='Password'
+                    onChange={onNewUserChange}
+                    value={newUser.password}
+                    required
+                />
                 <button type='submit'>Create an account</button>
             </form>
             <p>Already have an account? <span className='to-signin-page'>Sign in</span></p>
